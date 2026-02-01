5947bc898f2b7b51415309f8b62c5932d17dde18
Alexander P <you@example.com>
2026-02-01 11:08:52 +0100

    Gooogle analitic

---
+++ ga.js
@@ -0,0 +1,5 @@
+/* Google Analytics - external script so no inline script / no CSP unsafe-inline */
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-JD46LHFHMK');
